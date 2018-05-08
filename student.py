c5e168d599fe5e990e5947081fe59a2f50cb6326
Gabriel McColl <gabrielmccoll@users.noreply.github.com>
2018-05-08 15:06:51 +0100

    updated some classes

--- student.py
+++ student.py
@@ -1,6 +1,3 @@
-students = []
-
-
 class Student:
 	
 	school_name = "Springfield Elementary"
@@ -18,19 +15,3 @@ class Student:
 		
 	def get_school_name(self):
 		return self.school_name
-	
-	
-class HighSchoolStudent(Student): #derived class from parent
-	school_name = "Springfield High School"
-	
-	# method overides
-	def get_school_name(self):
-		return "This is high school"
-	
-	def get_name_capitalize(self):
-		original_value = super().get_name_capitalize() 
-		return original_value + " - HS"
-
-
-james = HighSchoolStudent("james")
-print(james.get_name_capitalize())
