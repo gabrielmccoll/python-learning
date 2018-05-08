39762ca37828980b6570763b765616a7246fb897
Gabriel McColl <gabrielmccoll@users.noreply.github.com>
2018-05-08 15:08:06 +0100

    added more classes

---
+++ hs_student.py
@@ -0,0 +1,10 @@
+class HighSchoolStudent(Student): #derived class from parent
+	school_name = "Springfield High School"
+	
+	# method overides
+	def get_school_name(self):
+		return "This is high school"
+	
+	def get_name_capitalize(self):
+		original_value = super().get_name_capitalize() 
+		return original_value + " - HS"
